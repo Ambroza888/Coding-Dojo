32f72af7d9529a055dfed399534e170af894c2e8
Ambroza888 <Ambroza8888@gmail.com>
2019-07-23 17:19:34 -0800

    This is my Jquary practise

---
+++ Week2/Jquery_practise/Jquery.js
@@ -0,0 +1,22 @@
+// $(".addclass button").click(function(){
+//   $(".addclass p").hide();
+// });
+// $(".addclass button").click(function(){
+//   $(".addclass p").addClass('papiP')
+// });
+
+// $(".slidetoggle button").click(function(){
+//   $(".slidetoggle p").slideToggle();
+// });
+
+// $(".append button").click(function(){
+//   $(".append p").append("PAPICHULIO");
+// });
+
+
+$(".slidetoggle .hide").click(function(){
+  $(".slidetoggle p").slideDown()
+});
+$(".slidetoggle .show").click(function(){
+  $(".slidetoggle p").slideUp()
+});
