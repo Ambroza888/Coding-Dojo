605c61dac955d21df86dd739c731d56900abc891
Ambroza888 <Ambroza8888@gmail.com>
2019-09-17 20:10:05 -0700

    more

--- Week5 MEAN stack/Angular/rate_my_cakes/controller.js
+++ Week5 MEAN stack/Angular/rate_my_cakes/controller.js
@@ -17,9 +17,9 @@ module.exports = {
     .catch(err => res.json(err));
   },
   update: function(req,res){
-    Cake.findOneAndUpdate({_id:req.params.id},{$push:{reviews:req.body}}) //{runValidators:true}
+    Cake.findOneAndUpdate({_id:req.params.id},{$push:{reviews:req.body}},{runValidators:true}) //{runValidators:true}
     .then(data=>res.json(data))
-    .catch(err=>res.json(data));
+    .catch(err=>res.json(err));
   },
   findById: function(req,res){
     Cake.findById({_id:req.params.id})
